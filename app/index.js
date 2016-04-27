d2109f380436db38f05766cfbaba4a747c8b7181
Brooks Forsyth <bforsyth@corp.chartercom.com>
2016-04-27 09:21:09 -0400

    Started next video

--- app/index.js
+++ app/index.js
@@ -1,6 +1,44 @@
+var USER_DATA = {
+    name: 'Brooks Forsyth',
+    username: 'bforsyth1234',
+    image: 'https://avatars0.githubusercontent.com/u/4528782'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Hello = React.createClass({
-  render: function () {return (<div>Hello ReactJS Program!</div>)}
+
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src = { this.props.imageUrl }
+        style = {
+            { height: 100, width: 100 }
+        }
+        />
+    }
 });
-ReactDOM.render(<Hello />, document.getElementById('app'));
+
+var profileLink = React.createClass({
+    render: function() {
+        return ( < div >
+	            	< a href = { 'https://www.github.com/' + this.props.username } >
+	            		{ this.props.username }
+	            	< /a>
+            	< /div>
+        )
+
+    }
+});
+
+var profileName = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.name}</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function
+})
+
+ReactDOM.render( < Hello name = "Brooks" anySortOfData = { 29 } / > , document.getElementById('app'));

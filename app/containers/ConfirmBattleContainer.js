042a2e64cbeaa3114389d6c5364e1dc5284012fa
Brooks Forsyth <bforsyth@corp.chartercom.com>
2016-05-23 10:11:11 -0400

    Video 7 finished

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,4 +1,6 @@
 var React = require('react');
+var githubHelpers = require('../utils/githubHelpers');
+
 var ConfirmBattle = require('../components/ConfirmBattle');
 
 var ConfirmBattleContainer = React.createClass({
@@ -15,6 +17,13 @@ var ConfirmBattleContainer = React.createClass({
 		var query = this.props.location.query;
 		// Feth info from github
 		//console.log('query', query );
+		githubHelpers.getPlayers([query.playerOne, query.playerTwo])
+			.then( function (players) {
+				this.setState({
+					isLoading: false,
+					playersInfo: [players[0], players[1]]
+				})
+			}.bind(this))
 	},
 	render: function() {
 		return (

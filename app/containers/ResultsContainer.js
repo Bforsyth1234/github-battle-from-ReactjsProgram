9398cdf6a71d7652ceb79d151aa180229704e589
Bforsyth1234 <brooks.forsyth@gmail.com>
2016-05-24 12:33:17 -0400

    Finished Video 9

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -2,21 +2,34 @@ var React = require('react');
 var Results = require('../components/Results');
 var githubHelpers = require('../utils/githubHelpers');
 
+
 var ResultsContainer = React.createClass({
-	getInitialState: funtion () {
+	getInitialState: function() {
 		return {
 			isLoading: true,
 			scores: []
 		}
 	},
 	componentDidMount: function() {
-		console.log(this.props)
+		console.log(this.props.location.state.playersInfo)
+        githubHelpers.battle(this.props.location.state.playersInfo)
+            .then(function (scores){
+                this.setState({
+                    scores: scores,
+                    isLoading: false
+                })
+            }.bind(this))
 	},
 	render: function() {
 		return(
-				<Results isLoading={this.state.isLoading} scores={this.state.scores} />
+			<div>
+				<Results
+                    isLoading={this.state.isLoading}
+                    playersInfo={this.props.location.state.playersInfo}
+                    scores={this.state.scores} />
+            </div>
 			)
 	}
-})
+});
 
 module.exports = ResultsContainer;
